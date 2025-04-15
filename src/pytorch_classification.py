0fc121b570eeb9dda4a2f75ed6077b12d824f3ad
Sang Young Noh <sangyoung123@gmail.com>
2025-04-15 14:15:05 +0100

    fix: cleaning up repo

--- src/pytorch_classification.py
+++ src/pytorch_classification.py
@@ -1,10 +1,25 @@
+
+import time
 import torch
 from torch import nn
 from tqdm import tqdm
+import torchvision
+from torchvision import transforms, datasets
 
-from sklearn.datasets import make_circles
+class FashionMNISTDataModule:
+    def __init__(self, batch_size = 64, resize = (28, 28), root = './data'):
+        self.batch_size = batch_size
+        self.resize = resize
+        self.root = root
+        self._prepare_transforms()
+        self._prepare_datasets()
 
-n_samples = 100
-X, y = make_circles(n_samples, noise = 0.03, random_state = 42)
+    def _prepare_transforms(self):
+        self.transform  = transforms.Compose([
+            transforms.Resize(self.resize),
+            transforms.ToTensor()
+            ])
+    
+    
 
 
